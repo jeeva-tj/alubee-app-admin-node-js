296f82c9ef2262c52519a7ce640639c8be4c8b31
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-21 18:23:23 +0530

    report router

---
+++ routers/report_router.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import {
+    report,
+    viewReport
+} from '../controllers/report_controller.js';
+const router = express.Router();
+import { userProtect } from '../middlewares/AuthMiddleware.js';
+
+
+router.route('/report').get(userProtect, report)
+router.route('/view-report').get(userProtect, viewReport)
+
+export default router;
