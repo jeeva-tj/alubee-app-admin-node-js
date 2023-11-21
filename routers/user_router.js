ef2487008616008b5f85dfd41b847b6a3a40e7db
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-21 12:35:10 +0530

    user router

--- routers/user_router.js
+++ routers/user_router.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import {
    login,
-   allUser
+   allUser,
+   profile,
+   newUser,
+   updateUser,
+   deleteUser,
+   userById,
+   resetPwd
 } from '../controllers/user_controller.js';
 const router = express.Router();
 import { userProtect } from '../middlewares/AuthMiddleware.js';
 
 router.route('/login').post(login)
-router.route('/user').get(userProtect, allUser)
+router.route('/profile').get(userProtect, profile)
+router.route('/user').get(userProtect, allUser).post(userProtect, newUser)
+router.route('/user/reset-pwd').put(userProtect, resetPwd)
+router.route('/user/:id').get(userProtect, userById).put(userProtect, updateUser).delete(userProtect, deleteUser)
+
 
 
 export default router;
