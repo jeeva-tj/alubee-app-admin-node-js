2b12a7570be939de3e19d798af1797d054a2563d
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-21 18:23:40 +0530

    work order router

---
+++ routers/workOrder_router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+    createWorkOrder,
+    deleteWorkOrder,
+    getWorkOrders, updateWorkOrder, workOrderById, workOrder_requirements
+} from '../controllers/workOrder_controller.js';
+const router = express.Router();
+import { userProtect } from '../middlewares/AuthMiddleware.js';
+
+
+router.route('/work-orders').get(userProtect, getWorkOrders)
+router.route('/work-order-req').get(userProtect, workOrder_requirements)
+router.route('/work-order-create').post(userProtect, createWorkOrder)
+router.route('/work-order/:id').get(userProtect, workOrderById).delete(userProtect, deleteWorkOrder).put(userProtect, updateWorkOrder)
+
+export default router;
