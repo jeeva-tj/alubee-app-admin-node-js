3ba5a1b137907808f97f326cda66b85dde9c74a6
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-21 12:34:49 +0530

    auth middleware

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -8,17 +8,37 @@ const userProtect = asyncHandler(async (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-
+    
         try {
 
             token = req.headers.authorization.split(' ')[1];
 
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            
+            const query = `SELECT User_ID, Email, Name, Role FROM alubee_dataset.alubee_user_table WHERE User_ID=${decoded.id}`;
+            
+            const user = await bigquery.query(query);
+            
+            req.user = user[0][0];
 
-            const query = `SELECT User_ID, Email, Name, Role FROM alubee_dataset.alubee_user_table WHERE User_ID='${decoded.id}'`;
+            let role = user[0][0].Role;
 
-            console.log(decoded);
-            // req.user = await bigquery.query(query);
+            if (req.originalUrl === '/v2/api/profile' && role === 'Owner') {
+                next();
+
+            } else if (req.originalUrl === '/v2/api/user' && role === 'Owner'){
+                next();
+
+            } else if (req.route.path === '/user/:id' && role === 'Owner') {
+                next();
+
+            } else if (req.originalUrl === '/v2/api/user/reset-pwd' && role === 'Owner'){
+                next();
+                
+            }else{
+                res.status(401)
+                throw Error("you can't access this api!")
+            }
             
             // next()
         } catch (err) {
