db18db162a8c7121f7515c5b7d2b66904140d8a1
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-22 12:50:32 +0530

    auth role

---
+++ middlewares/AuthRole.js
@@ -0,0 +1,48 @@
+
+import asyncHandler from 'express-async-handler';
+
+const authRoleProtect = asyncHandler(async(req, res, next) => {    
+    const user = req?.session?.user;
+
+    if (user) {
+        if (req.originalUrl === '/dashboard' && (user.role === 'Owner' || user.role === 'Editor' || user.role === 'Viewer')) {
+            next();
+            
+        } else if (req.originalUrl === '/reports' && (user.role === 'Owner' || user.role === 'Editor' || user.role === 'Viewer')){
+            next();
+
+        } else if (req.originalUrl === '/work-order' && (user.role === 'Owner' || user.role === 'Editor')){
+            next();
+
+        } else if (req.originalUrl === '/device-manager' && user.role === 'Owner'){
+            next();
+
+        } else if (req.originalUrl === '/user-management' && user.role === 'Owner'){
+            next();
+
+        }else {
+            res.redirect('/dashboard');
+        }
+
+    }else{
+        res.redirect('/');
+    }
+})
+
+
+const authLogin = asyncHandler(async (req, res) => {
+    const user = req?.session?.user;
+
+    if (user) {
+        res.redirect('/dashboard');
+
+    } else {
+        res.redirect('/');
+    }
+})
+
+
+export {
+    authRoleProtect,
+    authLogin
+}
