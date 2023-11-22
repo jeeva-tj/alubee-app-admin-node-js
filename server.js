421a9ebaa37681a42fe9260d0a855763e3a6e2d0
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-22 12:51:23 +0530

    server js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import path from 'path';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import viewRouter from './routers/view_router.js';
 import userRouter from './routers/user_router.js';
 import reportRouter from './routers/report_router.js';
@@ -15,13 +17,35 @@ dotenv.config();
 // express app
 const app = express();
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
 
-if (process.env.NODE_ENV !== 'production') {
-    app.use(morgan('dev'))
+//session middleware
+var session_config = {
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: {
+        maxAge: oneDay
+    },
+    resave: false
 }
+app.use(session(session_config));
 
+
+// cookie parser middleware
+app.use(cookieParser());
+
+
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'))
+// }
+
+if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    session_config.cookie.secure = true // serve secure cookies
+}
 
 const __dirname = path.resolve()
 
