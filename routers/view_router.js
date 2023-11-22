02ef9dad44b7197e676e648600abba3e0676c606
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-22 12:51:15 +0530

    delete

--- routers/view_router.js
+++ routers/view_router.js
@@ -8,13 +8,14 @@ import {
     userManagementPage
 } from '../controllers/view_controller.js';
 const router = express.Router();
+import { authRoleProtect } from '../middlewares/AuthRole.js';
 
 router.route('/').get(loginPage);
-router.route('/dashboard').get(dashboardPage);
-router.route('/reports').get(reportPage);
-router.route('/work-order').get(workOrderPage);
-router.route('/device-manager').get(deviceMangerPage);
-router.route('/user-management').get(userManagementPage);
+router.route('/dashboard').get(authRoleProtect, dashboardPage);
+router.route('/reports').get(authRoleProtect, reportPage);
+router.route('/work-order').get(authRoleProtect, workOrderPage);
+router.route('/device-manager').get(authRoleProtect, deviceMangerPage);
+router.route('/user-management').get(authRoleProtect, userManagementPage);
 
 
 export default router;
