0665b8d19b97368827f70d6ccd187b40c34983da
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-11-28 18:13:19 +0530

    profile script

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,8 +3,20 @@ const a_user = document.getElementById('a_user');
 const a_email = document.getElementById('a_email');
 const a_phone = document.getElementById('a_phone');
 const a_role = document.getElementById('a_role')
+
 const loader = document.getElementById('loader');
+const text_loader = document.getElementById('text_loader');
+
 const admin_profile = document.getElementById('admin_profile');
+
+const a_update_btn = document.getElementById('a_update_btn');
+
+const a_name_input = document.getElementById('a_name_input');
+const a_email_input = document.getElementById('a_email_input');
+const a_phone_input = document.getElementById('a_phone_input');
+
+const a_saveUpdate_btn = document.getElementById('a_saveUpdate_btn');
+const a_cancelUpdate_btn = document.getElementById('a_cancelUpdate_btn');
 
 
 const notyf = new Notyf();
@@ -39,6 +51,10 @@ async function getProfile() {
             a_phone.innerHTML = data.Phone;
             a_role.innerHTML = data.Role;
 
+            a_name_input.value = data.Name;
+            a_email_input.value = data.Email;
+            a_phone_input.value = data.Phone;
+
         } else {
 
             loader.classList.remove('active');
@@ -70,3 +86,106 @@ async function getProfile() {
 }
 
 getProfile();
+
+
+a_update_btn.addEventListener('click', () => {
+
+    a_name_input.classList.add('active');
+    a_email_input.classList.add('active');
+    a_phone_input.classList.add('active');
+
+    a_saveUpdate_btn.classList.add('active');
+    a_cancelUpdate_btn.classList.add('active');
+
+    a_update_btn.style.display = "none";
+    a_user.style.display = "none";
+    a_email.style.display = "none";
+    a_phone.style.display = "none";
+})
+
+a_saveUpdate_btn.addEventListener('click', async() => {
+
+    try {
+
+        text_loader.innerHTML = "Loading...!";
+
+        const a_data = {
+            name: a_name_input.value,
+            email: a_email_input.value,
+            phone: a_phone_input.value
+        }
+
+
+        const a_token = sessionStorage.getItem('alubee');
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${a_token}`
+            }
+        }
+
+        const { data } = await axios.put(`${hostUrl}/admin/profile`, a_data, config)
+
+        if (data.success === true) {
+            text_loader.innerHTML = "";
+
+            a_name_input.classList.remove('active');
+            a_email_input.classList.remove('active');
+            a_phone_input.classList.remove('active');
+
+            a_saveUpdate_btn.classList.remove('active');
+            a_cancelUpdate_btn.classList.remove('active');
+
+            a_update_btn.style.display = "block";
+            a_user.style.display = "block";
+            a_email.style.display = "block";
+            a_phone.style.display = "block";
+
+            getProfile();
+
+        } else {
+
+            text_loader.innerHTML = "";
+            notyf.error({
+                message: 'Something went wrong!',
+                duration: 5000,
+                position: {
+                    x: 'right',
+                    y: 'top',
+                },
+                dismissible: true
+            })
+        }
+        
+    } catch (error) {
+        
+        text_loader.innerHTML = "";
+        const resErr = error.response && error.response.data.message ? error.response.data.message : error.message
+        notyf.error({
+            message: resErr,
+            duration: 5000,
+            position: {
+                x: 'right',
+                y: 'top',
+            },
+            dismissible: true
+        })
+    }
+})
+
+
+
+a_cancelUpdate_btn.addEventListener('click', () => {
+    a_name_input.classList.remove('active');
+    a_email_input.classList.remove('active');
+    a_phone_input.classList.remove('active');
+
+    a_saveUpdate_btn.classList.remove('active');
+    a_cancelUpdate_btn.classList.remove('active');
+
+    a_update_btn.style.display = "block";
+    a_user.style.display = "block";
+    a_email.style.display = "block";
+    a_phone.style.display = "block";
+})
